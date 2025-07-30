7fdeb8e8c2948ac29af0b99404bf5f51f8610620
Ayholiii <oscar.obandov@upb.edu.co>
2025-07-30 15:35:56 -0500

    Inicio C#

--- 1.InicioVisualStudio/1.InicioVisualStudio/Program.cs
+++ 1.InicioVisualStudio/1.InicioVisualStudio/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Hola, Oscar Obando\n");
+            Console.Write("\tbienvenido a la clase de programacion");
         }
     }
 }
--- 1.InicioVisualStudio/1.InicioVisualStudio/obj/Debug/net8.0/1.InicioVisualStudio.AssemblyInfo.cs
+++ 1.InicioVisualStudio/1.InicioVisualStudio/obj/Debug/net8.0/1.InicioVisualStudio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1.InicioVisualStudio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f22edbb170034acfbcdf839e07f78048bb7fd70")]
 [assembly: System.Reflection.AssemblyProductAttribute("1.InicioVisualStudio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1.InicioVisualStudio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
