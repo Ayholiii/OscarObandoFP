5dee2914695c6d502f1739d5b8cb5cf07d616c0d
Ayholiii <oscar.obandov@upb.edu.co>
2025-08-01 15:23:43 -0500

    cariables, constatntes y tipo de datos

--- 1.InicioVisualStudio/1.InicioVisualStudio/Program.cs
+++ 1.InicioVisualStudio/1.InicioVisualStudio/Program.cs
@@ -4,8 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Hola, Oscar Obando\n");
+            //Esta es la declaración e inicialización de una variable
+            string nombre="daniel";
+            const string iva = "19%";
+
+            Console.Write("Hola, " + nombre + "\n");
             Console.Write("\tbienvenido a la clase de programacion");
+            nombre = "roberto";
+            Console.Write("\n" + nombre);
+            //iva = "20%"; NO se puede cambiar el valor de una constante
+
+            //Tipos de datos
+            byte dato1 = 255;
+            int dato2 = 202321;
+            long dato3 = 9202125235;
+            double dato4 = 326.625;
+            char dato5 = '$'; //Puede almacenar un solo caracter
+            bool dato6 = false; //Almacena true o false
+            object dato7 = new Program();
+
         }
     }
 }
--- 1.InicioVisualStudio/1.InicioVisualStudio/obj/Debug/net8.0/1.InicioVisualStudio.AssemblyInfo.cs
+++ 1.InicioVisualStudio/1.InicioVisualStudio/obj/Debug/net8.0/1.InicioVisualStudio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1.InicioVisualStudio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f22edbb170034acfbcdf839e07f78048bb7fd70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fdeb8e8c2948ac29af0b99404bf5f51f8610620")]
 [assembly: System.Reflection.AssemblyProductAttribute("1.InicioVisualStudio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1.InicioVisualStudio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
