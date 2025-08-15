50b132577b8bbb46a652958844af21423152b8c3
Daniel Obando <oscar.obandov@upb.edu.co>
2025-08-15 15:28:26 -0500

    5.Condiocionales Anidados

---
+++ 5.CondicionalAnidado/5.CondicionalAnidado/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _5.CondicionalAnidado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CONDICIONAL ANIDADO
+
+            /*Diseñar un algoritmo que lea tres números A, B, C y visualice en pantalla el valor más grande. El usuario debe ingresar tres valores diferentes.*/
+
+            int num1=0, num2=0, num3=0;
+
+            Console.WriteLine("ingrese el número 1: ");
+            num1=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("ingrese el número 2: ");
+            num2=Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingrese el número 3: ");
+            num3=Int32.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                if (num1 >num3)
+                {
+                    Console.WriteLine($"Número 1: {num1}, el es mayor");
+                }
+                else
+                {
+                    Console.WriteLine($"Número 3: {num3}, es el mayor");
+                }
+            }
+            else
+            {
+                if (num2 > num3)
+                {
+                    Console.WriteLine($"Número 2: {num2}, es el mayor");
+                }
+                else
+                {
+                    Console.WriteLine($"Número 3: {num3}, es el mayor");
+                }
+            }
+
+        }
+    }
+}
