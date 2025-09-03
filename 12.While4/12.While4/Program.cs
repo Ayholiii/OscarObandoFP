9b8f39faa6cf829ab80cde807474ae5cacae5fe5
Daniel Obando <oscar.obandov@upb.edu.co>
2025-09-03 14:11:50 -0500

    realizar

---
+++ 12.While4/12.While4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _12.While4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realizar algoritmo que permita obtener la suma de todos los números impares hasta el 99. 
+        }
+    }
+}
