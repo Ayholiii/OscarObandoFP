513af7e3a77a56d16cc79144439d416748b65b63
Daniel Obando <oscar.obandov@upb.edu.co>
2025-09-03 14:42:48 -0500

    13.While5PrimerEjercicio

---
+++ 13.While5/13.While5/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _13.While5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escribir un algoritmo que cuente cuántos números pares hay en un rango de números del 1 al número n. 
+
+          
+            {
+                int CantidadPares = 0, contador = 1, rango = 1;
+
+                Console.WriteLine("Ingrese un número : ");
+                rango = int.Parse(Console.ReadLine());
+
+                while (contador <= rango)
+                {
+                    if (contador % 2 == 0) // si es par
+                    {
+                        CantidadPares++;
+                    }
+                    contador++;
+                }
+
+                Console.WriteLine($"Cantidad de números pares entre 1 y {rango}, es: {CantidadPares}");
+
+                //Otra Forma
+            }
+        }
+
+    }
+}
+
