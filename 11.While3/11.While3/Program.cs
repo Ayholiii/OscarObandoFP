7a331e9521b6a33e98e4a32a9caf9194ea79112e
Daniel Obando <oscar.obandov@upb.edu.co>
2025-09-03 14:10:59 -0500

    falta realizar

---
+++ 11.While3/11.While3/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _11.While3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realizar un algoritmo que pida números enteros positivos y los sume, hasta que se ingrese un número entero negativo. Se debe mostrar por pantalla el total de la suma de los números ingresados. 
+
+        }
+    }
+}
