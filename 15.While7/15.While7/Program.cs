cc72dc86eed0d1057a75def8713810b25595102d
Daniel Obando <oscar.obandov@upb.edu.co>
2025-09-05 15:17:24 -0500

    17.DoWhile

---
+++ 15.While7/15.While7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection.Emit;
+
+namespace _15.While7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realizar un algoritmo para determinar cuánto ahorrará una persona en un año, si al final de cada mes deposita cantidades variables de dinero; además, se quiere saber cuánto lleva ahorrado cada mes. 
+        }
+    }
+}
---
+++ 17.DoWhile/17.DoWhile/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _17.DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Algoritmo que genera y suma los 5 primeros números enteros
+
+            /*int contador = 0, acumulador = 0;
+
+            do
+            {
+                contador++;
+                acumulador = acumulador + contador;
+            } while (contador < 5);
+
+            Console.WriteLine($"La suma de los 5 números enteros es: {acumulador}");*/
+
+            //Algoritmo que solicita un número y genere su correspondiente tabla de multiplicar desde el 1 hasta el 10. Y así sucesivamente hasta que el usuario ya no desee continuar generando tablas de multiplicar. 
+
+            int contador = 1, num = 0;
+            string respuesta = "";
+
+            do
+            {
+
+                Console.WriteLine("Ingrese un número para calcular su tabla de multiplicar");
+                num = int.Parse(Console.ReadLine());
+
+                do
+                {
+                    Console.WriteLine($"{num} x {contador} = {num * contador}");
+                    contador++;
+
+                } while (contador <= 10);
+
+                Console.WriteLine("Desea generar una nueva tabla de mutíplicar: S:si N:no");
+
+                respuesta = Console.ReadLine().ToLower();
+
+                contador = 1;
+
+            } while (respuesta == "s");
+
+
+        }
+    }
+}
