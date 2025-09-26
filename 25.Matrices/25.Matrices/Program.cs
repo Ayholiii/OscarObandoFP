ac11731201ea1447cb1e91a3956c7b7f69c91c25
Daniel Obando <oscar.obandov@upb.edu.co>
2025-09-26 15:10:38 -0500

    25.Matrices

---
+++ 25.Matrices/25.Matrices/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace _25.Matrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arreglos Bidimensionales - Matrices
+
+            int[,] numeros = new int[2, 2];
+
+            //string[,] nombres = new string[3, 2];
+
+           // char[,] caracteres = new char[2, 4];
+
+            bool[,] banderas = new bool[2, 5];
+
+            object[,] objetos = new object[2, 5];
+
+            // Asignacion de valores con f, c
+
+            numeros[0, 0] = 15;
+            numeros[0, 1] = 25;
+            numeros[1, 0] = 32;
+            numeros[1, 1] = 4;
+
+            // numeros [1, 2] = 7; error por índices fuera de los rangos
+
+            //Recuperar datos almacenados en una posición
+
+            Console.WriteLine($"valor almacenado en la posici[on con índices 1,0: {numeros[1, 0]} ");
+
+
+            //Declaración e inicialización de matrices con valores por defect
+
+            string[,] apellidos =
+            {
+                {"Chica", "López", "Álvarez" },
+                {"Patiño","Ciro","Castaño" },
+                {"Duque","Carmona","Castaño" }
+            };
+
+            //Recorrer Para Almacenar Datos 
+
+           /* string[,] nombres = new string[3, 2];
+
+            for (int i = 0; i < 3; i++) // recorre filas
+            {
+
+                for (int j = 0; j < 2; j++) // recorre columnas
+                {
+
+                    Console.WriteLine($"Ingrese el nombre para la posición {i},{j}: ");
+
+                    nombres[i, j] = Console.ReadLine();
+
+                }
+
+            }
+
+            //recorrer para recuperar datos
+
+            for (int i = 0; i < nombres.GetLength(0); i++) // .getLength(0) devuelve el número de filas de la matriz
+            {
+                for (int j = 0; j < nombres.GetLength(1); j++) // .getLength(1) devuelve el número de columnas
+                {
+                    Console.WriteLine($"{nombres[i, j]} |");
+                }
+            }*/
+
+            char[,] caracteres = new char[2, 4];
+
+            for (int i = 0; i < 2; i++) // recorre filas
+            {
+
+                for (int j = 0; j < 4; j++) // recorre columnas
+                {
+
+                    Console.WriteLine($"Ingrese un caracter para la posición {i},{j}: ");
+
+                    caracteres[i, j] = Convert.ToChar(Console.ReadLine());
+
+                }
+
+            }
+
+            //recorrer para recuperar datos
+
+            for (int i = 0; i < caracteres.GetLength(0); i++) // .getLength(0) devuelve el número de filas de la matriz
+            {
+                for (int j = 0; j < caracteres.GetLength(1); j++) // .getLength(1) devuelve el número de columnas
+                {
+                    Console.Write($"{caracteres[i, j]} |");
+                }
+            }
+        }
+    }
+}
---
+++ TallerVectores/TallerVectores/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace TallerVectores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Taller Vectores
+
+            /*1. Escribir un algoritmo que permita llenar un vector[15] con números enteros, y luego 
+                 encuentre y muestre el valor máximo y mínimo de los números ingresados. 
+
+            int[] vector = new int[15];
+            int maximo;
+            int minimo;
+
+            console.WriteLine("Ingrese en cada posición un número DIFERENTE");
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.Write($"Ingrese un número en la posición {i + 1}: ");
+                vector[i] = int.Parse(Console.ReadLine());
+            }
+
+            maximo = vector[0];
+            minimo = vector[0];
+
+            for (int i = 1; i < vector.Length; i++)
+            {
+                if (vector[i] > maximo)
+                {
+                    maximo = vector[i];
+                }
+                if (vector[i] < minimo)
+                {
+                    minimo = vector[i];
+                }
+            }
+            Console.WriteLine($"El valor máximo es: {maximo}");
+            Console.WriteLine($"El valor mínimo es: {minimo}");
+            */
+
+            /*2. Escribe un algoritmo que permita ingresar caracteres en un vector, y luego invierta el 
+                 orden de los elementos del vector. Se deben mostrar lo dos vectores.*/
+
+            char[] vector = new char[5];
+            char[] vectorInvertido = new char[5];
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.Write($"Ingrese un carácter en la posición {i + 1}: ");
+                vector[i] = char.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vectorInvertido[i] = vector[vector.Length - 1 - i];
+            }
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.WriteLine(vector[i]);
+            }
+
+            for (int i = 0; i < vectorInvertido.Length; i++)
+            {
+                Console.WriteLine(vectorInvertido[i]);
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
