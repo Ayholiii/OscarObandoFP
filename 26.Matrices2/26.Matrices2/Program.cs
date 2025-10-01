7602e25bd2df371540b524b8b4b836345c62d299
Daniel Obando <oscar.obandov@upb.edu.co>
2025-10-01 15:32:58 -0500

    TallerMatrices
    Completar.
    corregir punto 2

---
+++ 26.Matrices2/26.Matrices2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _26.Matrices2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*26. Escribe un programa que realice la suma de dos matrices de dimensiones 2x3. 
+
+                  Requisitos del programa: 
+
+                   1. Solicita al usuario que ingrese los elementos de la primera matriz de 2 filas y 3 columnas. 
+                   2. Solicita al usuario que ingrese los elementos de la segunda matriz de las mismas dimensiones (2x3). 
+                   3. Calcula la matriz suma, resultado de sumar cada elemento correspondiente de las dos matrices. 
+                   4. Muestra la matriz resultante de la suma en formato de matriz (2 filas, 3 columnas). 
+            */
+
+        }
+
+    }
+}
---
+++ TallerMatrices/TallerMatrices/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace TallerMatrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Taller Matrices
+            /*Desarrollar un programa que crea una matriz de 10 filas y 20 columnas y muestre por 
+              pantalla la suma de los elementos de cada columna. 
+
+            int[,] matriz = new int[10, 20];
+            int[] sumaColumnas = new int[20];
+
+            Console.WriteLine("Ingrese los elementos de la matriz (10 filas y 20 columnas):");
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    Console.Write($"Elemento [{i + 1},{j + 1}]: ");
+                    matriz[i, j] = int.Parse(Console.ReadLine());
+                    sumaColumnas[j] += matriz[i, j];
+                }
+            }
+            Console.WriteLine($"suma de cada columna:");
+            for (int j = 0; j < 20; j++)
+            {
+                Console.WriteLine($"Columna {j + 1}: {sumaColumnas[j]}");
+            }
+            Console.ReadLine();
+
+            //ahora con random 
+
+            int[,] matriz = new int[10, 20];
+            int[] sumaColumnas = new int[20];
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    matriz[i, j] = rand.Next(1, 101); // Números aleatorios entre 1 y 100
+                    //mostrar la matriz por filas y columnas
+                    Console.Write($"{matriz[i, j]} |"); // Formateo para mejor visualización
+                    sumaColumnas[j] += matriz[i, j];
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine($"suma de cada columna:");
+            for (int j = 0; j < 20; j++)
+            {
+                Console.WriteLine($"Columna {j + 1}: {sumaColumnas[j]}");
+            }
+            */
+
+
+            /* Desarrollar un programa que crea una matriz de n filas * m columnas, el usuario ingresa 
+               caracteres en cada posición de la matriz hasta llenarla. El programa debe intercambiar la 
+               primera fila con la última fila de la matriz. Al final se debe imprimir la matriz original, y la 
+               matriz con el intercambio de filas.*/
+
+            int n = 0;
+            int m = 0;
+            char[,] matriz = new char[n, m];
+
+            Console.Write("Ingrese el número de filas que desee ingresar: ");
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el número de columnas que desee ingresar: ");
+            m = int.Parse(Console.ReadLine());
+
+            matriz = new char[n, m];
+            Console.WriteLine("Ingrese los caracteres para llenar la matriz:");
+ 
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write($"Elemento [{i + 1},{j + 1}]: ");
+                    matriz[i, j] = char.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Matriz Original:");
+            Console.WriteLine(matriz);
+
+
+
+
+
+
+
+
+        }
+    }
+}
