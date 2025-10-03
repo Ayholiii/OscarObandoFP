73ca55a7186d54b902671eb0b976115b626a9a80
Daniel Obando <oscar.obandov@upb.edu.co>
2025-10-03 15:26:55 -0500

    29.Modular1
    //completar viernes en clase

---
+++ 29.Modular1/29.Modular1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _29.Modular1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] multiplosDe5 = new int[CapturarColumnas(), CapturarColumnas()];
+        }
+
+        static int CapturarFilas ()
+        {
+            Console.WriteLine("Ingrese la cantidad de filas para la matriz:");
+            int filas = int.Parse(Console.ReadLine());
+            return filas;
+        }
+
+        static int CapturarColumnas()
+        {
+            Console.WriteLine("Ingrese la cantidad de Columnas para la matriz:");
+            int columnas = int.Parse(Console.ReadLine());
+            return columnas;
+        }
+
+        static int[,] LlenarMatriz(int[,] matriz)
+        {
+            return matriz;
+        }
+    }
+}
