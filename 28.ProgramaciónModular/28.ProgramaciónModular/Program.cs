526be1057f61b054e959a3ffe0ec6eeb829860d7
Daniel Obando <oscar.obandov@upb.edu.co>
2025-10-03 15:14:50 -0500

    28.Programación Modular

---
+++ 28.ProgramaciónModular/28.ProgramaciónModular/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _28.ProgramaciónModular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MostrarMensaje();
+            MostrarMensajes("Soy Danielle");
+            MostrarMensajes($"tengo {CalcularEdad()} años");
+            MostrarMensajes($"Tengo {CalcularEdad(2025, 2007)}");
+        }
+
+        //Módulo 1 - Procedimiento sin parámetros
+
+        static void MostrarMensaje()
+        {
+            Console.WriteLine("Bienvenido a la programación modular");
+            MostrarMensajes("estoy siendo llamado desde MostrarMensaje()");
+        }
+
+        //Módulo 2 - 
+
+        static void MostrarMensajes(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+        }
+
+        //Módulo 3 - Función sin parámetros
+
+        static int CalcularEdad ()
+        {
+            int edad = 2025 - 2007;
+            return edad;
+        }
+
+        //Módulo 4 - Función con parámetros
+
+        static int CalcularEdad(int AñoActual, int AñoNacimiento)
+        {
+            return AñoActual - AñoNacimiento;
+        }
+    }
+}
