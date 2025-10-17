a2f9eeefbf78c2b7826a80bc9500838efca7a9e9
Daniel Obando <oscar.obandov@upb.edu.co>
2025-10-17 14:26:48 -0500

    23.Vectores

--- 23.Vectores2/23.Vectores2/Program.cs
+++ 23.Vectores2/23.Vectores2/Program.cs
@@ -7,6 +7,16 @@
             //Crear arreglo llamado "numeros" de 100 elementos asignar el número 10 en cada una de las posiciones del arreglo. Leer el contenido de cada elemento y escribirlo en pantalla.
 
             int[] numeros = new int[100];
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = 10;
+            }
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+          
+
         }
     }
 }
