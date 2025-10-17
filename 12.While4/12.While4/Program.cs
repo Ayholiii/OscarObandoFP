bcba499c597de92252bde110bf45a736fd07b168
Daniel Obando <oscar.obandov@upb.edu.co>
2025-10-17 14:44:07 -0500

    12.While4

--- 12.While4/12.While4/Program.cs
+++ 12.While4/12.While4/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             //Realizar algoritmo que permita obtener la suma de todos los números impares hasta el 99. 
+            int numero = 1;
+            int suma = 0;
+            while (numero < 100)
+            {
+                suma += numero;
+                numero += 2;
+            }
+            Console.WriteLine("La suma de los números impares hasta el 99 es: " + suma);
+
         }
     }
 }
