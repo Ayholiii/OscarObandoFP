95cd161b8c0f28d9d02d44016840bf80b2019a11
Daniel Obando <oscar.obandov@upb.edu.co>
2025-10-17 14:30:08 -0500

    18.DoWhile

--- 18.DoWhile2/18.DoWhile2/Program.cs
+++ 18.DoWhile2/18.DoWhile2/Program.cs
@@ -9,10 +9,37 @@
                    2. Si el saldo es menor a 3’000.000, sacar un mensaje que diga el nombre del usuario, el número de cuenta, el saldo y “No es apto para el crédito” 
                    El algoritmo debe permitir preguntar la información de usuarios, hasta que se le indique que ya no se desea preguntar más. 
                    Además, el algoritmo debe permitir mostrar el número de usuarios a los que se le pregunto por la información, y debe mostrar el promedio de los saldos. */
-
-
-
- 
+            
+            string nombre, numeroCuenta, opcion;
+            double saldo, sumaSaldos = 0;
+            int contadorUsuarios = 0;
+            
+            do
+            {
+                Console.Write("Ingrese el nombre del usuario: ");
+                nombre = Console.ReadLine();
+                Console.Write("Ingrese el número de cuenta: ");
+                numeroCuenta = Console.ReadLine();
+                Console.Write("Ingrese el saldo: ");
+                saldo = Convert.ToDouble(Console.ReadLine());
+                sumaSaldos += saldo;
+                contadorUsuarios++;
+                if (saldo > 3000000)
+                {
+                    Console.WriteLine($"Usuario: {nombre}, Número de cuenta: {numeroCuenta}, Saldo: {saldo}, Es apto para el crédito");
+                }
+                else
+                {
+                    Console.WriteLine($"Usuario: {nombre}, Número de cuenta: {numeroCuenta}, Saldo: {saldo}, No es apto para el crédito");
+                }
+                Console.Write("¿Desea ingresar otro usuario? (s/n): ");
+                opcion = Console.ReadLine().ToLower();
+            } while (opcion == "s");
+            double promedioSaldos = sumaSaldos / contadorUsuarios;
+            Console.WriteLine($"Número de usuarios ingresados: {contadorUsuarios}");
+            Console.WriteLine(
+                $"Promedio de saldos: {promedioSaldos}");
+           
         }
     }
 }
