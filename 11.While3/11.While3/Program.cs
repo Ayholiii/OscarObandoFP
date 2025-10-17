a535ecf6862a3b27a73b8f0e6c62c28710c6a773
Daniel Obando <oscar.obandov@upb.edu.co>
2025-10-17 14:45:12 -0500

    11.while3

--- 11.While3/11.While3/Program.cs
+++ 11.While3/11.While3/Program.cs
@@ -6,6 +6,22 @@
         {
             //Realizar un algoritmo que pida números enteros positivos y los sume, hasta que se ingrese un número entero negativo. Se debe mostrar por pantalla el total de la suma de los números ingresados. 
 
+            int suma = 0;
+            int numero;
+            
+            while (true)
+            {
+                Console.Write("Ingrese un número entero positivo (o un número negativo para finalizar): ");
+                numero = int.Parse(Console.ReadLine());
+                if (numero < 0)
+                {
+                    break;
+                }
+                suma += numero;
+            }
+            Console.WriteLine($"La suma total de los números ingresados es: {suma}");
+
+
         }
     }
 }
